aa0ecbba8c17f6a4760e560d1889e3b3f8cd9d1d
Anik Sarker <aniksarker1612@gmail.com>
2020-08-04 07:36:11 +0800

    contagion section added

---
+++ app.js
@@ -0,0 +1,10 @@
+// const card_deck = document.getElementsByClassName('card-deck');
+// const card = document.getElementsByClassName('card');
+
+// for (let i = 0; i < card.length; i++) {
+//     var cardClicked = card[i];
+//     cardClicked.addEventListener('click', function(event){
+//         cardClicked = event.target;
+//         card[i].style.borderBottom = '4px solid #FA5652';
+//     })
+// }
